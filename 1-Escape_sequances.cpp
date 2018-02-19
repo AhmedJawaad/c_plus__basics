1e4b46eeea38d8be98d12865b63efb34e9dd7780
Ahmed <ahmed887463@gmail.com>
2018-02-19 10:07:35 +0200

    commeted

--- 1-Escape_sequances.cpp
+++ 1-Escape_sequances.cpp
@@ -1,20 +1,29 @@
 /*
  
- Made by Ahmed Ab Al Aziz 2/2018
+ Made by Ahmed Ab Al Aziz Abd Al Hadi => 2/2018
  
  */
 
 #include <iostream> 
-using namespace std;
-// the main function where the programme starts from
+    using namespace std;
 
-int main() {
+
+    int main() { // the main function where the programme starts from
     // \n new line
     cout << "this statment will followed by 6 new line \n\n\n\n\n\n";
+    
+    
+    
     //\t new Tab  
     cout << "This Statment will take new tab \t like that \n";
+    
+    
+    
     //\a alarm
     cout << "this Statment will makeComputer beep \a \n";
+    
+    
+    
 
     // the backslashes cause print the next letter
     // print backslashe \\ print single quate \' print double coute \"
@@ -25,6 +34,9 @@ int main() {
 
     // \r make the cursor 
     cout << "the cursor will be at the begining of this line \r";
+    
+    
+    
 
     // \b cursor take one back space left
     cout << "make the cursor stop the letter before the last letter \b";
@@ -34,5 +46,5 @@ int main() {
 
     return 0;
     // the return is 0 cause main function return int and it mean stop the programme
-
-}
+      
+    }//End Of Main Function

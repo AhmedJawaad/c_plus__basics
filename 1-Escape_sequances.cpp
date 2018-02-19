9cde6b5d69f142965371b384c7fb072af1dcc70b
Ahmed <ahmed887463@gmail.com>
2018-02-19 09:16:06 +0200

    commeted

--- 1-Escape_sequances.cpp
+++ 1-Escape_sequances.cpp
@@ -1,31 +1,38 @@
+/*
+ 
+ Made by Ahmed Ab Al Aziz 2/2018
+ 
+ */
+
 #include <iostream> 
 using namespace std;
 // the main function where the programme starts from
-int main(){  
-// \n new line
-    cout<<"this statment will followed by 6 new line \n\n\n\n\n\n";
-     //\t new Tab  
-  cout<<"This Statment will take new tab \t like that \n";
-  //\a alarm
-  cout<<"this Statment will makeComputer beep \a \n";
- 
-   // the backslashes cause print the next letter
-  // print backslashe \\ print single quate \' print double coute \"
-  
- cout<<"\\ \' \" \n"; 
-  
-  
-  
-  // \r make the cursor 
-  cout<<"the cursor will be at the begining of this line \r";
-  
+
+int main() {
+    // \n new line
+    cout << "this statment will followed by 6 new line \n\n\n\n\n\n";
+    //\t new Tab  
+    cout << "This Statment will take new tab \t like that \n";
+    //\a alarm
+    cout << "this Statment will makeComputer beep \a \n";
+
+    // the backslashes cause print the next letter
+    // print backslashe \\ print single quate \' print double coute \"
+
+    cout << "\\ \' \" \n";
+
+
+
+    // \r make the cursor 
+    cout << "the cursor will be at the begining of this line \r";
+
     // \b cursor take one back space left
-  cout<<"make the cursor stop the letter before the last letter \b";
+    cout << "make the cursor stop the letter before the last letter \b";
 
 
-    
-    
-    return 0; 
+
+
+    return 0;
     // the return is 0 cause main function return int and it mean stop the programme
-    
+
 }
